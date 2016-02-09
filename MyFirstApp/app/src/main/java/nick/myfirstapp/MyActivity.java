5902faaf19ae4d8631c6567c11482a3837d549f8
nickrunner <nick.a.schrock@gmail.com>
2016-02-09 16:53:41 +0000

    added files

---
+++ MyFirstApp/app/src/main/java/nick/myfirstapp/MyActivity.java
@@ -0,0 +1,68 @@
+package nick.myfirstapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+
+public class MyActivity extends ActionBarActivity {
+    public final static String EXTRA_MESSAGE = "nick.MyFirstApp.MESSAGE";
+
+    /**Called when user clicks Send button*/
+    //method must be public
+    //must have a void return value
+    //must have a View as the only parameter
+    public void sendMessage(View view){
+        //Do something in response to button push
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void openSearch(){
+        //Write what happens when search is opened
+    }
+
+    public void openSettings(){
+        //Write what happens when settings is opened
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch(item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
